8ff3e68d3463439ac02ec621e1f030d22b920db4
Yauheni Ralko <yauheni_ralko@epam.com>
2017-09-24 21:35:11 +0300

    The task has been partially implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function zeros(expression) {
-  // your solution
+  var factorial = expression.split('*');
+  var counter = 0;
+  var fives = 0;
+  var hasEven = false;
+
+  factorial.forEach (val => {
+	var num = parseInt(val);
+
+	if(val.includes('!!')){
+	if(num % 2) {
+	  fives += Math.round(num / 10) + Math.round(num / 25);
+
+	} else {
+	  hasEven = true;
+	  counter += Math.floor(num/10);
+	}
+
+	} else {
+	  var total = Math.floor(num/5) + Math.floor(num/25);
+	  counter += total;
+	}
+})
+
+  if(hasEven && fives){
+    counter += fives;
+  }
+
+  return counter;
 }
